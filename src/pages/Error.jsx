0b52196f9052b819674ed04020ae738f77928b29
thatFemicode <leyedavid10@gmail.com>
2021-09-03 19:17:37 +0100

    finished project and final commit

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,18 @@
-import React from "react";
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 const Error = () => {
-  return <div></div>;
+  return (
+    <div>
+      <section className="error-page">
+        <div className="error-container">
+          <h1>😡 This URL address does not exist, Go back home</h1>
+          <Link to="/" className="btn btn-block">
+            BACK HOME
+          </Link>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Error;
