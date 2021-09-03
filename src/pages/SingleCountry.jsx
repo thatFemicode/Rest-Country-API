d994574432ac1f4820b44e85a33cc91c48cd466b
thatFemicode <leyedavid10@gmail.com>
2021-09-03 18:29:14 +0100

    fixed infinite loop with useeffect

--- src/pages/SingleCountry.jsx
+++ src/pages/SingleCountry.jsx
@@ -6,8 +6,8 @@ import { useGlobalContext } from '../components/context/context';
 import Loading from '../components/Loading';
 const url = `https://restcountries.eu/rest/v2/name`;
 const SingleCountry = () => {
-  const { name } = useParams();
-  const oName = name.replace(/-/, '');
+  let { name } = useParams();
+  const oName = name.replace(/-/g, ' ');
   // console.log(name);
   const { loading, setLoading } = useGlobalContext();
   const [country, setCountry] = useState([]);
@@ -15,40 +15,8 @@ const SingleCountry = () => {
     try {
       const response = await fetch(`${url}/${oName}`);
       const data = await response.json();
-      if (data) {
-        const {
-          numericCode,
-          name,
-          flag,
-          nativeName,
-          population,
-          region,
-          subregion,
-          capital,
-          topLevelDomain,
-          currencies,
-          languages,
-          borders,
-        } = data[0];
-        const newCountry = {
-          numericCode,
-          name,
-          flag,
-          nativeName,
-          population,
-          region,
-          subregion,
-          capital,
-          topLevelDomain,
-          currencies,
-          languages,
-          borders,
-        };
-        setCountry(newCountry);
-      } else {
-        setCountry([]);
-      }
       setLoading(false);
+      setCountry(data);
     } catch (error) {
       console.log(error);
     }
@@ -61,10 +29,93 @@ const SingleCountry = () => {
   if (loading) {
     return <Loading />;
   }
+
+  // const populate = population.toLocaleString();
+
   return (
-    <div>
-      <h2>{name}</h2>
-    </div>
+    <section className="country-section">
+      <div className="container">
+        <Link to="/">
+          <button className="btn back-btn">
+            <FaArrowLeft className="icon" />
+            Back
+          </button>
+        </Link>
+        <div>
+          {country.map((item) => {
+            const {
+              numericCode,
+              main,
+              flag,
+              nativeName,
+              population,
+              region,
+              subregion,
+              capital,
+              topLevelDomain,
+              currencies,
+              languages,
+              borders,
+            } = item;
+            console.log(item);
+            return (
+              <article key={numericCode} className="single-country">
+                <div className="flag">
+                  <img src={flag} alt={main} />
+                </div>
+                <div className="single-country-info">
+                  <h4>{main}</h4>
+                  <div className="top-section">
+                    <div className="inenr-top">
+                      <p>
+                        Native name: <span>{nativeName}</span>
+                      </p>
+                      <p>
+                        Population: <span>{population.toLocaleString()}</span>
+                      </p>
+                      <p>
+                        Region: <span>{region}</span>
+                      </p>
+                      <p>
+                        Sub Region: <span>{subregion}</span>
+                      </p>
+                      <p>
+                        Capital: <span>{capital}</span>
+                      </p>
+                    </div>
+                    <div className="inner-bottom">
+                      <p>
+                        Top Level Domain: <span>{topLevelDomain}</span>
+                      </p>
+                      <p>
+                        Currencies: <span>{currencies[0].name}</span>
+                      </p>
+                      <p>
+                        Languages: <span>{languages[0].name}</span>
+                      </p>
+                    </div>
+                  </div>
+                  {borders.length > 0 && (
+                    <div className="border-section">
+                      <h5>Border Countries</h5>
+                      <div className="bd-container">
+                        {borders.map((border, index) => {
+                          return (
+                            <p key={index} className="text">
+                              {border}
+                            </p>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </section>
   );
 };
 
